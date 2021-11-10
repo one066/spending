45566e0353bac8b12bcad7ef466b72b78771d1c6
one <49442264+one066@users.noreply.github.com>
2021-11-10 13:33:14 +0800

    增加发送详情 (#29)

--- src/timed_task/task1.py
+++ src/timed_task/task1.py
@@ -1,33 +1,10 @@
-import datetime
-
-import pandas as pd
-from dateutil.relativedelta import relativedelta
-
-from apps.back_end.models import RecordSpending, User
-from extension.mysql_client import db
-from SDK.email import OneEmail
-
-
-def get_title():
-    last_time = datetime.datetime.strftime(
-        (datetime.datetime.now() - relativedelta(months=+1)), '%Y-%m-%d')
-    now_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
-    return f'{last_time}---{now_time}'
+import requests
 
 
 def task1():
     """ 定时发邮箱
     """
-    df = pd.read_sql(
-        '''select title, people, price, start_time from record_spending where status is null''',
-        db.engine.raw_connection())
-    df.to_excel('apps/front_end/static/data.xlsx', encoding='utf-8')
-    title = get_title()
-
-    # 发送邮件
-    OneEmail().every_mouth_data(users=User.emails(), title=title)
-
-    # 更新数据库
-    RecordSpending.query.filter(RecordSpending.status == '暂无').update(
-        {'status': title})
-    db.session.commit()
+    response = requests.get(
+        'http://127.0.0.1:5000/v1/service/send_every_mouth_user_spending')
+    response.raise_for_status()
+    print(response.json())
