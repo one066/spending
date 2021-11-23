3746fcf35c57600041094c08710eadceb3dbadce
one <49442264+one066@users.noreply.github.com>
2021-11-23 10:43:23 +0800

    测试打包 (#47)

--- src/configs.py
+++ src/configs.py
@@ -4,6 +4,7 @@ from extension.base_config import BaseConfig
 class Development(BaseConfig):
     STAGE = 'development'
 
+    # local 激活定时任务api
     SEND_EVERY_MOUTH_USER_SPENDING_URL = 'HTTP://127.0.0.1:5000/v1/service/send_every_mouth_user_spending'
 
     REDIS_HOST = '127.0.0.1'
@@ -13,7 +14,9 @@ class Production(BaseConfig):
     DEBUG = False
     STAGE = 'production'
 
+    # service 激活定时任务api
     SEND_EVERY_MOUTH_USER_SPENDING_URL = 'https://121.43.135.49/v1/service/send_every_mouth_user_spending'
     REDIS_HOST = 'redis'
 
+    # docker-compose SQLALCHEMY_DATABASE_URI
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456hk@mysql:3306/spending?charset=utf8'
