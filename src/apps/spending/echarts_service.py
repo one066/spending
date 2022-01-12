1ad9ae9cb52a727d44b631b740967adfd1290ec5
one <49442264+one066@users.noreply.github.com>
2022-01-12 16:56:24 +0800

    修复 折线图api bug (#74)

--- src/apps/spending/echarts_service.py
+++ src/apps/spending/echarts_service.py
@@ -4,7 +4,8 @@ import pandas as pd
 from flask import Blueprint
 from sqlalchemy import func
 
-from apps.spending.models import RecordSpending, User
+from apps.spending.models import RecordSpending as Rs
+from apps.spending.models import User
 from apps.spending.util import build_every_mouth_body, get_now_mouth_title
 from apps.spending.validator import (LineDataSerialize, PieValidator,
                                      ShowSpendingSerialize,
@@ -26,9 +27,9 @@ class ShowSpending(GetView):
     serialize_class = ShowSpendingSerialize
 
     def action(self, *arg, **kwargs):
-        _record_spending = RecordSpending.query.filter_by(
+        _record_spending = Rs.query.filter_by(
             status=self.validated_data['status']).order_by(
-                RecordSpending.start_time.desc()).all()
+                Rs.start_time.desc()).all()
         return {'data': [record.show() for record in _record_spending]}
 
 
@@ -38,7 +39,7 @@ class SpendingGroupByUser(GetView):
     serialize_class = SpendingGroupByUserSerialize
 
     def action(self, *arg, **kwargs):
-        group_spending = RecordSpending.get_spending_group_by_user(
+        group_spending = Rs.get_spending_group_by_user(
             self.validated_data['status'])
 
         # 满足 echart.js 参数条件
@@ -57,8 +58,7 @@ class Status(GetView):
     NOW_STATUS = '暂无'
 
     def action(self):
-        status = db.session.query(RecordSpending.status).group_by(
-            RecordSpending.status).all()
+        status = db.session.query(Rs.status).group_by(Rs.status).all()
         status = [_st.status for _st in status]
 
         # 排除当前月份的
@@ -68,32 +68,37 @@ class Status(GetView):
         return {'status': status}
 
 
-@class_route(echarts_service, '/line_data')
-class LineData(GetView):
+@class_route(echarts_service, '/user_spending_by_date')
+class UserSpendingByDate(GetView):
     validated_class = StatusValidator
     serialize_class = LineDataSerialize
 
     @staticmethod
-    def _get_user_spending_by_status_and_date(user, status, date):
-        return RecordSpending.query.filter(
-            RecordSpending.status == status, RecordSpending.people == user,
-            RecordSpending.start_time.like(f'{date}%')).order_by(
-                RecordSpending.start_time.desc()).all()
-
-    @staticmethod
     def _get_series_data(records) -> float:
-        return sum([float('%.2f' % record.price)
-                    for record in records]) if records else 0
+        return '%.2f' % sum([float(record.price)
+                             for record in records]) if records else 0
 
     @staticmethod
     def _get_dates_by_status(status: str) -> List[str]:
         dates = db.session.query(
             func.date_format(
-                RecordSpending.start_time,
+                Rs.start_time,
                 '%Y-%m-%d').label('date')).group_by('date').filter(
-                    RecordSpending.status == status).order_by(
-                        RecordSpending.start_time.asc()).all()
-        return [date.start_time for date in dates]
+                    Rs.status == status).order_by(Rs.start_time.asc()).all()
+
+        return [date.date for date in dates]
+
+    @staticmethod
+    def _get_user_spending_by_date(user, dates, status):
+        user_spending_by_date = []
+        for date in dates:
+            records = Rs.query.filter(Rs.status == status, Rs.people == user,
+                                      Rs.start_time.like(f'{date}%')).order_by(
+                                          Rs.start_time.desc()).all()
+            user_date_spending = '%.2f' % sum(
+                [float(record.price) for record in records]) if records else 0
+            user_spending_by_date.append(user_date_spending)
+        return user_spending_by_date
 
     def action(self, *arg, **kwargs):
         status = self.validated_data['status']
@@ -104,16 +109,14 @@ class LineData(GetView):
         # 满足 echart.js 参数条件
         series = []
         for user in users:
-            for date in dates:
-                records = self._get_user_spending_by_status_and_date(
-                    user, status, date)
-                series_data = self._get_series_data(records)
-                series.append({
-                    'name': user,
-                    'type': 'line',
-                    'stack': 'Total',
-                    'data': series_data
-                })
+            user_spending = self._get_user_spending_by_date(
+                user, dates, status)
+            series.append({
+                'name': user,
+                'type': 'line',
+                'stack': 'Total',
+                'data': user_spending
+            })
 
         return {
             'dates': dates,
@@ -129,8 +132,8 @@ class SendEveryMouthUserSpending(GetView):
     def _save_file(self):
         # TODO 没发现 api 暂时先保存下来、后面读取文件发送
         user_spending = db.session.query(
-            RecordSpending.title, RecordSpending.people, RecordSpending.price,
-            RecordSpending.start_time).filter_by(status='暂无').all()
+            Rs.title, Rs.people, Rs.price,
+            Rs.start_time).filter_by(status='暂无').all()
 
         df = pd.DataFrame(user_spending,
                           columns=['title', 'name', 'price', 'start_time'])
@@ -141,7 +144,7 @@ class SendEveryMouthUserSpending(GetView):
         self._save_file()
 
         # 发送邮件
-        group_spending = RecordSpending.get_spending_group_by_user('暂无')
+        group_spending = Rs.get_spending_group_by_user('暂无')
         now_mouth_title = get_now_mouth_title()
         every_mouth_body = build_every_mouth_body(group_spending)
 
@@ -155,7 +158,6 @@ class SendEveryMouthUserSpending(GetView):
         one_email.send()
 
         # 更新数据库
-        RecordSpending.query.filter(RecordSpending.status == '暂无').update(
-            {'status': now_mouth_title})
+        Rs.query.filter(Rs.status == '暂无').update({'status': now_mouth_title})
         db.session.commit()
         return
