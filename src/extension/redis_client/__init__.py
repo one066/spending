c4d44c5d4dfa92147beafbbda0e286ae646f60f7
one <49442264+one066@users.noreply.github.com>
2022-03-23 14:46:19 +0800

    修复bug (#84)

--- src/extension/redis_client/__init__.py
+++ src/extension/redis_client/__init__.py
@@ -3,4 +3,5 @@ import redis
 from extension.project_config import get_config
 
 redis_host = get_config().REDIS_HOST
-redis_client = redis.Redis(host=redis_host, port=6379, db=0)
+redis_password = get_config().REDIS_PASSWORD
+redis_client = redis.Redis(host=redis_host, port=6379, db=0, password=redis_password,)
