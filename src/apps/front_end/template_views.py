a807132b886eb874e781d966f65779688f58abfa
one <49442264+one066@users.noreply.github.com>
2022-06-14 11:15:34 +0800

    自定义备案号 (#90)

--- src/apps/front_end/template_views.py
+++ src/apps/front_end/template_views.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Blueprint, redirect, render_template, url_for
 
 from extension.flask.api import func_check_token_v1
@@ -11,32 +13,34 @@ front_end_views = Blueprint(
     static_folder='static'
 )
 
+record_number = os.environ.get("RECORD_NUMBER")
+
 
 @front_end_views.route('/login', methods=['GET'])
 def login():
 
-    return render_template('login.html')
+    return render_template('login.html', record_number=record_number)
 
 
 @front_end_views.route('/home', methods=['GET'])
 @func_check_token_v1
 def home():
 
-    return render_template('home.html')
+    return render_template('home.html', record_number=record_number)
 
 
 @front_end_views.route('/show', methods=['GET'])
 @func_check_token_v1
 def show():
 
-    return render_template('show.html')
+    return render_template('show.html', record_number=record_number)
 
 
 @front_end_views.route('/history', methods=['GET'])
 @func_check_token_v1
 def history():
 
-    return render_template('history.html')
+    return render_template('history.html', record_number=record_number)
 
 
 @front_end_views.route('/', methods=['GET'])
@@ -48,4 +52,4 @@ def init():
 @front_end_views.app_errorhandler(404)
 def page_not_found(e):
 
-    return render_template('404.html')
+    return render_template('404.html', record_number=record_number)
